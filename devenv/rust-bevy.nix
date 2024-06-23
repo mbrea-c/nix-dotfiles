5f57f17ea6ccda1779a90b8a4104581f18a01cd0
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-23 14:18:27 +0100

    adding .devs seemed to do something

--- devenv/rust-bevy.nix
+++ devenv/rust-bevy.nix
@@ -3,17 +3,17 @@
     name = "rust-bevy-fhs";
     targetPkgs = pkgs: with pkgs; [
         pkg-config
-        udev
-        alsa-lib
-        vulkan-loader
-        xorg.libX11
-        xorg.libXrandr
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXtst
-        libxkbcommon
-        wayland
-        openssl
+        udev.dev
+        alsa-lib.dev
+        vulkan-loader.dev
+        xorg.libX11.dev
+        xorg.libXrandr.dev
+        xorg.libXcursor.dev
+        xorg.libXi.dev
+        xorg.libXtst.dev
+        libxkbcommon.dev
+        wayland.dev
+        openssl.dev
         systemd.dev
     ];
     # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ buildInputs);
