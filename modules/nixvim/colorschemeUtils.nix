07e8ddf407a19254a49d8c8cbaf9dbd97875548e
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-24 23:30:07 +0100

    fix

--- modules/nixvim/colorschemeUtils.nix
+++ modules/nixvim/colorschemeUtils.nix
@@ -2,5 +2,5 @@
   background = { palette, ... }: palette.base00;
   mapColors = lib.attrsets.mapAttrs (_: hl:
     lib.attrsets.mapAttrs
-    (key: value: if key == "fg" || key == "bg" then "#${value}" else value));
+    (key: value: if key == "fg" || key == "bg" then "#${value}" else value) hl);
 }
