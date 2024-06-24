6d95c1fcf6a4e24525ebe9b25d5b4cc7e4e7642b
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-24 20:26:54 +0100

    Add lsps to nvim

--- modules/nixvim-standalone.nix
+++ modules/nixvim-standalone.nix
@@ -1,3 +1,10 @@
 { config, lib, pkgs, inputs, ... }:
 {
+    plugins.lsp = {
+        enable = true;
+        servers = {
+            nil-ls.enable = true;
+            rust-analyzer.enable = true;
+        };
+    };
 }
