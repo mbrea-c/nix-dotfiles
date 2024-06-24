9250d68ac3428d0c3170689dd537c61a1103dd70
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-24 23:14:27 +0100

    fix colorscheme typo

--- modules/nixvim/colorscheme.nix
+++ modules/nixvim/colorscheme.nix
@@ -4,8 +4,8 @@ let
   palette = colorscheme.palette;
 in {
   "@note" = {
-    fg = u.background colorscheme;
-    bg = palette.base05;
+    fg = "#${u.background colorscheme}";
+    bg = "#${palette.base05}";
   };
-  "@comment" = { fg = palette.base08; };
+  "@comment" = { fg = "${palette.base08}"; };
 }
