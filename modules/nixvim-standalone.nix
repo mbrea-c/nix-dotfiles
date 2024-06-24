a17fd11654215ca6b34ead5db41e3f89c977b65a
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-24 20:29:15 +0100

    Add autocomplete to nvim

--- modules/nixvim-standalone.nix
+++ modules/nixvim-standalone.nix
@@ -7,4 +7,14 @@
             rust-analyzer.enable = true;
         };
     };
+
+    plugins.nvim-cmp = {
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+        ];
+    };
 }
