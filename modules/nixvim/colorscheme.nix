b2b811627c91f08605fec44a73b98f31d81a65a1
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-24 23:37:16 +0100

    comment out colors for now

--- modules/nixvim/colorscheme.nix
+++ modules/nixvim/colorscheme.nix
@@ -3,9 +3,9 @@ let
   u = (import ./colorschemeUtils.nix) { inherit lib; };
   palette = colorscheme.palette;
 in u.mapColors {
-  "@note" = {
-    fg = u.background colorscheme;
-    bg = palette.base05;
-  };
-  "@comment" = { fg = palette.base08; };
+  # "@note" = {
+  #   fg = u.background colorscheme;
+  #   bg = palette.base05;
+  # };
+  # "@comment" = { fg = palette.base08; };
 }
