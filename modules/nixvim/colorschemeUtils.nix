63ce44de4a5b20813e351235f2882a3b566216c3
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-25 19:30:38 +0100

    add custom decToHex for two decimal places

--- modules/nixvim/colorschemeUtils.nix
+++ modules/nixvim/colorschemeUtils.nix
@@ -73,30 +73,18 @@
       b = builtins.floor(rgb.b * 255 + 0.5);
     };
 
-
-    decToHex' =
+    decToHex = dec:
       let
+        dHigh = dec / 16;
+        dLow = lib.mod dec 16;
         intToHex = [
           "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"
           "a" "b" "c" "d" "e" "f"
         ];
-        toHex' = q: a:
-          if q > 0
-          then (toHex'
-            (q / 16)
-            ((builtins.elemAt intToHex (lib.mod q 16)) + a))
-          else a;
+        hHigh = builtins.elemAt intToHex dHigh;
+        hLow = builtins.elemAt intToHex dLow;
       in
-        v: toHex' v "";
-
-    decToHex = dec:
-      let
-        hex = decToHex' dec;
-      in
-        if builtins.stringLength hex == 1 then
-          "0${hex}"
-        else
-          hex;
+        "${hHigh}${hLow}";
 
     fromRGB = rgb:
       let
