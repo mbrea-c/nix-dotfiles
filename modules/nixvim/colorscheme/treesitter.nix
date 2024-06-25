9f78638b8b1c00e804e34fc419574f64be47ee0e
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-25 19:44:25 +0100

    comment out to debug

--- modules/nixvim/colorscheme/treesitter.nix
+++ modules/nixvim/colorscheme/treesitter.nix
@@ -3,27 +3,27 @@ let
   u = (import ../colorschemeUtils.nix) { inherit lib inputs; };
   palette = colorscheme.palette;
 in {
-  "@note" = {
-    fg = u.background colorscheme;
-    bg = palette.base05;
-  };
-  "@comment" = { fg = palette.base08; };
-  "@comment.documentation" = {
-    fg = u.lighten palette.base08 0.2;
-    italic = true;
-  };
-  "@warning" = {
-    fg = u.background colorscheme;
-    bg = palette.base05;
-  };
-  "@danger" = {
-    fg = u.background colorscheme;
-    bg = palette.base03;
-  };
-  "@constructor" = { fg = palette.base06; };
-  "@conditional" = { fg = palette.base02; };
-  "@constant" = {
-    fg = u.darken palette.base06 0.1;
-    bold = true;
-  };
+  # "@note" = {
+  #   fg = u.background colorscheme;
+  #   bg = palette.base05;
+  # };
+  # "@comment" = { fg = palette.base08; };
+  # "@comment.documentation" = {
+  #   fg = u.lighten palette.base08 0.2;
+  #   italic = true;
+  # };
+  # "@warning" = {
+  #   fg = u.background colorscheme;
+  #   bg = palette.base05;
+  # };
+  # "@danger" = {
+  #   fg = u.background colorscheme;
+  #   bg = palette.base03;
+  # };
+  # "@constructor" = { fg = palette.base06; };
+  # "@conditional" = { fg = palette.base02; };
+  # "@constant" = {
+  #   fg = u.darken palette.base06 0.1;
+  #   bold = true;
+  # };
 }
