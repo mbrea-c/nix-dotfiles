b291e98194fe5fb81520c18d48bc98a83b9ff3b2
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-25 19:25:46 +0100

    fix

--- modules/nixvim/colorschemeUtils.nix
+++ modules/nixvim/colorschemeUtils.nix
@@ -2,10 +2,10 @@
   background = { palette, ... }: palette.base00;
 
   listMax = list:
-    builtins.foldl (a: b: if a > b then a else b) (builtins.elemAt 0 list)
+    lib.foldl (a: b: if a > b then a else b) (builtins.elemAt list 0)
     list;
   listMin = list:
-    builtins.foldl (a: b: if a < b then a else b) (builtins.elemAt 0 list)
+    lib.foldl (a: b: if a < b then a else b) (builtins.elemAt list 0)
     list;
 
   mapColors = lib.attrsets.mapAttrs (_: hl:
