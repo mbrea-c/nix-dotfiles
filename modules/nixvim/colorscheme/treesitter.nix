754a3ff117526765dcbea475794729e5c5d2a0ab
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-25 19:46:27 +0100

    uncomment some stuff

--- modules/nixvim/colorscheme/treesitter.nix
+++ modules/nixvim/colorscheme/treesitter.nix
@@ -3,11 +3,11 @@ let
   u = (import ../colorschemeUtils.nix) { inherit lib inputs; };
   palette = colorscheme.palette;
 in {
-  # "@note" = {
-  #   fg = u.background colorscheme;
-  #   bg = palette.base05;
-  # };
-  # "@comment" = { fg = palette.base08; };
+  "@note" = {
+    fg = u.background colorscheme;
+    bg = palette.base05;
+  };
+  "@comment" = { fg = palette.base08; };
   # "@comment.documentation" = {
   #   fg = u.lighten palette.base08 0.2;
   #   italic = true;
