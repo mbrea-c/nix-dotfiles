aed7317cef66acb02e18eefad2e923a5c6338189
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-25 20:53:20 +0100

    fix

--- modules/nixvim/colorscheme.nix
+++ modules/nixvim/colorscheme.nix
@@ -3,5 +3,5 @@ let
   u = (import ./colorschemeUtils.nix) { inherit lib inputs; };
   args = { inherit lib colorscheme inputs; };
 in u.mapColors ((import ./colorscheme/base.nix args)
-  ++ (import ./colorscheme/treesitter.nix args)
-  ++ (import ./colorscheme/lsp-semantic-highlight.nix args))
+  // (import ./colorscheme/treesitter.nix args)
+  // (import ./colorscheme/lsp-semantic-highlight.nix args))
