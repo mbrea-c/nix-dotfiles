d0c231b1a823a84a0ad64c522e3d1ca1e35e477a
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-27 21:38:39 +0100

    diagnostics

--- modules/nixvim/colorscheme.nix
+++ modules/nixvim/colorscheme.nix
@@ -4,4 +4,5 @@ let
   args = { inherit lib colorscheme inputs; };
 in u.mapColors ((import ./colorscheme/base.nix args)
   // (import ./colorscheme/treesitter.nix args)
-  // (import ./colorscheme/lsp-semantic-highlight.nix args))
+  // (import ./colorscheme/lsp-semantic-highlight.nix args)
+  // (import ./colorscheme/diagnostic.nix args))
---
+++ modules/nixvim/colorscheme/diagnostic.nix
@@ -0,0 +1,48 @@
+{ lib, inputs, colorscheme, ... }:
+let
+  u = (import ../colorschemeUtils.nix) { inherit lib inputs; };
+  colors = u.fromBase16 colorscheme;
+  inherit (u) lighten darken saturate;
+  error = saturate 0.3 (lighten 0.3 colors.red);
+  warning = saturate 0.3 (lighten 0.3 colors.yellow);
+  info = lighten 0.5 colors.foreground;
+  hint = saturate 0.3 (lighten 0.3 colors.blue);
+in {
+  "DiagnosticError" = {
+    bg = darken 0.1 colors.background;
+    fg = error;
+  };
+  "DiagnosticWarn" = {
+    bg = darken 0.1 colors.background;
+    fg = warning;
+  };
+  "DiagnosticInfo" = {
+    bg = darken 0.1 colors.background;
+    fg = info;
+  };
+  "DiagnosticHint" = {
+    bg = darken 0.1 colors.background;
+    fg = hint;
+  };
+  "DiagnosticUnnecessary" = { link = "DiagnosticHint"; };
+  "DiagnosticUnderlineError" = {
+    undercurl = true;
+    sp = error;
+  };
+  "DiagnosticUnderlineWarn" = {
+    undercurl = true;
+    sp = warning;
+  };
+  "DiagnosticUnderlineInfo" = {
+    undercurl = true;
+    sp = info;
+  };
+  "DiagnosticUnderlineHint" = {
+    undercurl = true;
+    sp = hint;
+  };
+  "DiagnosticSignError" = { fg = error; };
+  "DiagnosticSignWarn" = { fg = warning; };
+  "DiagnosticSignInfo" = { fg = info; };
+  "DiagnosticSignHint" = { fg = hint; };
+}
