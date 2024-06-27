3beba545b9cafc3a04fa34b26afc873efa446db9
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-27 23:31:07 +0100

    cmp mappings

--- modules/nixvim-standalone.nix
+++ modules/nixvim-standalone.nix
@@ -7,25 +7,7 @@ let
   highlights =
     (import ./nixvim/colorscheme.nix) { inherit lib colorscheme inputs; };
 in {
-  keymaps = [
-    {
-      key = "<C-n>";
-      action = ":Neotree toggle<CR>";
-    }
-    {
-      key = ";f";
-      action = ":lua vim.lsp.buf.format()<CR>";
-    }
-    {
-      key = "<C-h>";
-      action = "<cmd>Inspect<CR>";
-    }
-    {
-      key = "<C-t>";
-      action = "<cmd>ToggleTerm<CR>";
-      mode = [ "n" "t" ];
-    }
-  ];
+  keymaps = import ./nixvim/keymap.nix;
 
   highlightOverride = highlights;
 
@@ -121,8 +103,37 @@ in {
     cmp = {
       enable = true;
       autoEnableSources = true;
-      settings.sources =
-        [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
+      settings = {
+        sources =
+          [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-n>" = # lua
+            ''
+              cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                else
+                  cmp.complete()
+                end
+              end, { "i", "s" })
+            '';
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<ESC>" = # lua
+            ''
+              cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.abort()
+                  -- Emulate <Esc> keypress after exiting, so that you're not left in
+                  -- insert mode
+                  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
+                else
+                  fallback()
+                end
+              end, { "i", "c" })
+            '';
+        };
+      };
     };
     none-ls = {
       enable = true;
---
+++ modules/nixvim/keymap.nix
@@ -0,0 +1,84 @@
+let
+  desc = d: { desc = d; };
+  lua = l: { __raw = l; };
+in [
+  {
+    key = "<C-n>";
+    action = ":Neotree toggle<CR>";
+  }
+  {
+    key = "<C-h>";
+    action = "<cmd>Inspect<CR>";
+  }
+  {
+    key = "<C-t>";
+    action = "<cmd>ToggleTerm<CR>";
+    mode = [ "n" "t" ];
+  }
+
+  # LSP stuff
+  {
+    key = "<leader>f";
+    action = lua "vim.lsp.buf.format";
+  }
+  {
+    mode = [ "n" ];
+    key = "gD";
+    action = lua "vim.lsp.buf.declaration";
+    options = desc "Go to declaration";
+  }
+  {
+    mode = [ "n" ];
+    key = "gd";
+    action = lua "vim.lsp.buf.definition";
+    options = desc "Go to definition";
+  }
+  {
+    mode = [ "n" ];
+    key = "gT";
+    action = lua "vim.lsp.buf.type_definition";
+    options = desc "Go to type definition";
+  }
+  {
+    mode = [ "n" ];
+    key = "K";
+    action = lua "vim.lsp.buf.hover";
+    options = desc "Hover";
+  }
+  {
+    mode = [ "n" ];
+    key = "gi";
+    action = lua "vim.lsp.buf.implementation";
+    options = desc "Go to implementation";
+  }
+  {
+    mode = [ "n" "i" ];
+    key = "<C-k>";
+    action = lua "vim.lsp.buf.signature_help";
+    options = desc "Signature help";
+  }
+  {
+    mode = [ "n" ];
+    key = "<leader>rn";
+    action = lua "vim.lsp.buf.rename";
+    options = desc "Rename";
+  }
+  {
+    mode = [ "n" ];
+    key = "<leader>ca";
+    action = lua "vim.lsp.buf.code_action";
+    options = desc "Code actions";
+  }
+  {
+    mode = [ "n" ];
+    key = "<leader>gr";
+    action = lua "vim.lsp.buf.references";
+    options = desc "References";
+  }
+  {
+    mode = [ "n" ];
+    key = "<leader>e";
+    action = lua "vim.diagnostic.open_float";
+    options = desc "Open diagnostics float";
+  }
+]
