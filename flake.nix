b3984cefa7b9a65900d0f727b454285e9cdd9369
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-28 00:22:21 +0100

    color

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      colorscheme = inputs.nix-colors.colorSchemes.spacemacs;
+      colorscheme = inputs.nix-colors.colorSchemes.standardized-dark;
     in {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs colorscheme; };
