5ed28d7d564a7c453ebe39d735fa957bdf89e26b
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-27 22:20:08 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-color-utils = {
-      url = ./../nix-color-utils;
+      url = "./../nix-color-utils";
       inputs.nix-colors.follows = "nix-colors";
     };
   };
