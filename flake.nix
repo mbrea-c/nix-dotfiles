6a94f92af250f3a9588bc26661114384244ef1bb
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-28 00:15:55 +0100

    more

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      colorscheme = inputs.nix-colors.colorSchemes.catppuccin-frappe;
+      colorscheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
     in {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs colorscheme; };
