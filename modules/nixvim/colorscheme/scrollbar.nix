818b62ce727c575bcb99c4cf49826f8ce53d2bd0
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-29 20:38:44 +0100

    update and fix

--- modules/nixvim/colorscheme/scrollbar.nix
+++ modules/nixvim/colorscheme/scrollbar.nix
@@ -2,12 +2,14 @@
 let
   u = inputs.nix-color-utils.lib;
   colors = u.fromBase16 colorscheme;
-  inherit (u.hsv) lighten darken saturate;
+  inherit (u.hsv) lighten saturate;
   error = saturate 0.3 (lighten 0.3 colors.red);
   warning = saturate 0.3 (lighten 0.3 colors.yellow);
   info = lighten 0.5 colors.foreground;
   hint = saturate 0.3 (lighten 0.3 colors.blue);
 in {
+  # The *Handle colors are applier when the sign is on top of the handle
+  # Otherwise, the regular group is applied
   "ScrollbarHandle" = { bg = colors.color8; };
   "ScrollbarCursorHandle" = {
     fg = colors.foreground;
