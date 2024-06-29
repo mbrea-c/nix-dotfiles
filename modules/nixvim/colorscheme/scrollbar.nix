d5a7210849fcdb3851cb5ba66eb2e01386fd5afb
Manuel Brea <m.brea.carreras@gmail.com>
2024-06-29 20:30:59 +0100

    scrollbar colors

---
+++ modules/nixvim/colorscheme/scrollbar.nix
@@ -0,0 +1,28 @@
+{ inputs, colorscheme, ... }:
+let
+  u = inputs.nix-color-utils.lib;
+  colors = u.fromBase16 colorscheme;
+  inherit (u.hsv) lighten darken saturate;
+  error = saturate 0.3 (lighten 0.3 colors.red);
+  warning = saturate 0.3 (lighten 0.3 colors.yellow);
+  info = lighten 0.5 colors.foreground;
+  hint = saturate 0.3 (lighten 0.3 colors.blue);
+in {
+  "ScrollbarHandle" = { bg = colors.color8; };
+  "ScrollbarErrorHandle" = {
+    fg = error;
+    bg = colors.color8;
+  };
+  "ScrollbarWarnHandle" = {
+    fg = warning;
+    bg = colors.color8;
+  };
+  "ScrollbarInfoHandle" = {
+    fg = info;
+    bg = colors.color8;
+  };
+  "ScrollbarHintHandle" = {
+    fg = hint;
+    bg = colors.color8;
+  };
+}
