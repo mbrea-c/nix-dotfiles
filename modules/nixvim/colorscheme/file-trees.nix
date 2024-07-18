d760e05ce8a1931d16e2b806d5ed1f0cbbefc3c6
Manuel Brea <m.brea.carreras@gmail.com>
2024-07-18 19:39:53 +0100

    colors

---
+++ modules/nixvim/colorscheme/file-trees.nix
@@ -0,0 +1,12 @@
+{ inputs, colorscheme, ... }:
+let
+  u = inputs.nix-color-utils.lib;
+  colors = u.fromBase16 colorscheme;
+in {
+  "Directory" = {
+    fg = u.saturate 0.2 colors.blue;
+    bold = true;
+  };
+  "NeoTreeDirectoryIcon" = { link = "Directory"; };
+  "NeoTreeDirectoryName" = { link = "Directory"; };
+}
--- modules/nixvim/colorscheme/neotree.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, colorscheme, ... }:
-let
-  u = inputs.nix-color-utils.lib;
-  colors = u.fromBase16 colorscheme;
-in {
-  "NeoTreeDirectoryIcon" = { fg = colors.blue; };
-  "NeoTreeDirectoryName" = { fg = colors.blue; };
-}
