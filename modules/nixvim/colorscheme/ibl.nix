4253fe83a365b350242377f4f42a936180274ce5
Manuel Brea <m.brea.carreras@gmail.com>
2024-07-19 19:18:41 +0100

    fix

--- modules/nixvim/colorscheme/ibl.nix
+++ modules/nixvim/colorscheme/ibl.nix
@@ -3,8 +3,9 @@ let
   u = inputs.nix-color-utils.lib;
   colors = u.fromBase16 colorscheme;
 in {
+  "IblIndent" = { fg = colors.color8; };
   "IblScope" = {
-    fg = colors.color8;
+    fg = u.hsv.lighten 0.15 colors.color8;
     bold = true;
   };
 }
