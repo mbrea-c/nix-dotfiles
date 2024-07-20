534dd9d3e8a59c66023ad5d072c4107c99aad867
Manuel Brea <m.brea.carreras@gmail.com>
2024-07-20 18:05:14 +0100

    remove dead code

--- modules/nixvim/plugins.nix
+++ modules/nixvim/plugins.nix
@@ -30,61 +30,6 @@
         };
       };
     };
-    # cmp = {
-    #   enable = true;
-    #   autoEnableSources = true;
-    #   settings = {
-    #     window = {
-    #       completion = {
-    #         col_offset = -3;
-    #         side_padding = 0;
-    #       };
-    #     };
-    #     formatting = {
-    #       fields = [ "kind" "abbr" "menu" ];
-    #       # format = # lua
-    #       #   ''
-    #       #     function(entry, vim_item)
-    #       #       local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-    #       #       local strings = vim.split(kind.kind, "%s", { trimempty = true })
-    #       #       kind.kind = " " .. (strings[1] or "") .. " "
-    #       #       kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-    #       #       return kind
-    #       #     end
-    #       #   '';
-    #     };
-    #     sources =
-    #       [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
-    #     mapping = {
-    #       "<CR>" = "cmp.mapping.confirm({ select = true })";
-    #       "<C-n>" = # lua
-    #         ''
-    #           cmp.mapping(function(fallback)
-    #             if cmp.visible() then
-    #               cmp.select_next_item()
-    #             else
-    #               cmp.complete()
-    #             end
-    #           end, { "i", "s" })
-    #         '';
-    #       "<C-p>" = "cmp.mapping.select_prev_item()";
-    #       "<ESC>" = # lua
-    #         ''
-    #           cmp.mapping(function(fallback)
-    #             if cmp.visible() then
-    #               cmp.abort()
-    #               -- Emulate <Esc> keypress after exiting, so that you're not left in
-    #               -- insert mode
-    #               vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
-    #             else
-    #               fallback()
-    #             end
-    #           end, { "i", "c" })
-    #         '';
-    #     };
-    #   };
-    # };
     treesitter = {
       enable = true;
       settings = { highlight.enable = true; };
