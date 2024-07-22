56cfc4d9c99f98446e1922c5feec4af58b9af4f0
Manuel Brea Carreras <mmanuelc@amazon.com>
2024-07-22 15:35:01 +0100

    fix

--- modules/nixvim-standalone.nix
+++ modules/nixvim-standalone.nix
@@ -156,4 +156,11 @@ in {
 
   plugins = plugins.plugins;
   extraPlugins = plugins.extraPlugins;
+  extraConfigLuaPre = let
+    border = "rounded";
+    # lua
+  in ''
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = "${border}"}),
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = "${border}"}),
+  '';
 }
