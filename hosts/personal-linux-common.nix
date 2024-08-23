fbf1024131af3c06f061fd81aad0e1103869317b
Manuel Brea <m.brea.carreras@gmail.com>
2024-08-23 19:33:42 +0100

    fix

--- hosts/personal-linux-common.nix
+++ hosts/personal-linux-common.nix
@@ -77,6 +77,7 @@ in {
   #   XDG_RUNTIME_DIR = "/run/user/$UID";
   # };
 
+  programs.ssh.startAgent = true;
   programs.zsh.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
