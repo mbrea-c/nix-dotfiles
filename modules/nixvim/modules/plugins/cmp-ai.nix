5e30d6910a68e2a9cb2cb7264e69345865ef110b
Manuel Brea <m.brea.carreras@gmail.com>
2024-09-18 19:31:32 +0100

    fix

--- modules/nixvim/modules/plugins/cmp-ai.nix
+++ modules/nixvim/modules/plugins/cmp-ai.nix
@@ -3,7 +3,7 @@
     cmp-ai = {
       enable = true;
       settings = {
-        max_lines = 100;
+        max_lines = 10;
         provider = "Ollama";
         provider_options = { model = "codellama:7b-code"; };
         run_on_every_keystroke = true;
