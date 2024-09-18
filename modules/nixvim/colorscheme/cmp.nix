87b8a6effa9aec751ddbdd2e66886a2db9a2814d
Manuel Brea <m.brea.carreras@gmail.com>
2024-09-18 19:12:31 +0100

    fix colors

--- modules/nixvim/colorscheme/cmp.nix
+++ modules/nixvim/colorscheme/cmp.nix
@@ -3,4 +3,33 @@ let
   u = inputs.nix-color-utils.lib;
 
   colors = u.fromBase16 colorscheme;
-in { }
+  inherit (u.hsv) darken mix desaturate;
+in {
+  "PMenu" = { link = "NormalFloat"; };
+  "CmpGhostText" = { fg = colors.color8; };
+  "CmpItemAbbrDeprecated" = {
+    strikethrough = true;
+    bg = colors.color8;
+  };
+  "CmpItemAbbrMatch" = { fg = colors.blue; };
+  "CmpItemAbbrMatchFuzzy" = { fg = colors.blue; };
+  "CmpItemMenu" = {
+    fg = colors.green;
+    italic = true;
+  };
+  "CmpItemKindVariable" = { bg = darken 0.3 colors.cyan; };
+  "CmpItemKindInterface" = { bg = darken 0.3 colors.cyan; };
+  "CmpItemKindText" = { bg = darken 0.2 colors.green; };
+  "CmpItemKindFunction" = { bg = darken 0.2 colors.magenta; };
+  "CmpItemKindMethod" = { bg = darken 0.2 colors.magenta; };
+  "CmpItemKindKeyword" = { bg = darken 0.1 colors.green; };
+  "CmpItemKindProperty" = { bg = darken 0.3 colors.cyan; };
+  "CmpItemKindUnit" = { fg = colors.foreground; };
+  "CmpItemKindSnippet" = { bg = darken 0.3 colors.green; };
+  "CmpItemKindCopilot" = {
+    bg = darken 0.3 (desaturate 0.3 (mix 0.5 colors.red colors.blue));
+  };
+  "CmpItemKindCodeium" = {
+    bg = darken 0.3 (desaturate 0.3 (mix 0.5 colors.red colors.blue));
+  };
+}
