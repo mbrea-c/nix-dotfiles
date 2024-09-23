8b45898ee85d9f8def9b06e3432097b0936e527f
Manuel Brea <m.brea.carreras@gmail.com>
2024-09-23 18:18:36 +0100

    fix

---
+++ modules/nixvim/colorscheme/aerial.nix
@@ -0,0 +1,14 @@
+{ inputs, colorscheme, ... }:
+let
+  u = inputs.nix-color-utils.lib;
+  colors = u.fromBase16 colorscheme;
+  inherit (u.hsv) darken;
+in {
+  "AerialLine" = {
+    fg = u.hsv.saturate 0.2 colors.blue;
+    bold = true;
+  };
+  "AerialFunctionIcon" = { fg = darken 0.1 colors.magenta; };
+  "AerialStructIcon" = { fg = darken 0.1 colors.red; };
+  "AerialClassIcon" = { fg = darken 0.1 colors.green; };
+}
