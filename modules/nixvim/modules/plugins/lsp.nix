3f04bce880f47df30ba25aacf73ab5f0705d7071
Manuel Brea <m.brea.carreras@gmail.com>
2024-10-28 21:59:37 +0000

    expand macro stuff

--- modules/nixvim/modules/plugins/lsp.nix
+++ modules/nixvim/modules/plugins/lsp.nix
@@ -39,4 +39,39 @@
       };
     };
   };
+
+  keymaps = [{
+    action = "<cmd>lua expand_macro()<CR>";
+    key = "<leader>em";
+  }];
+
+  extraConfigLuaPre = ''
+    local function expand_macro()
+      local params = vim.lsp.util.make_position_params()
+      vim.lsp.buf_request_all(0, "rust-analyzer.expandMacroRecursively", params, function(err, result)
+        if err then
+          vim.notify("Error expanding macro: " .. err.message, vim.log.levels.ERROR)
+          return
+        end
+        if result then
+          local buf = vim.api.nvim_create_buf(false, true)  -- Create a new buffer, non-listed and scratch
+          -- Set the lines (text) in the buffer
+          vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result, '\n'))
+          -- Define window options (position, size, etc.)
+          local width = 30
+          local height = 5
+          local opts = {
+              style = "minimal",    -- "minimal" style to avoid extra UI elements
+              relative = "editor",  -- Position relative to the editor
+              width = width,
+              height = height,
+              row = (vim.o.lines - height) / 2,     -- Center the floating window vertically
+              col = (vim.o.columns - width) / 2,    -- Center the floating window horizontally
+              border = "rounded"    -- Rounded border for aesthetics
+          }
+          vim.api.nvim_open_win(buf, true, opts)
+        end
+      end)
+    end
+  '';
 }
