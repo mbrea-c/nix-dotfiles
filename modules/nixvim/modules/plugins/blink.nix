2794d26c644e9646435c78db1f76d245edb19749
Manuel Brea Carreras <m.brea.carreras@gmail.com>
2024-12-29 13:02:12 +0000

    fix

--- modules/nixvim/modules/plugins/blink.nix
+++ modules/nixvim/modules/plugins/blink.nix
@@ -3,13 +3,13 @@
     blink-cmp = {
       enable = true;
       settings = {
-        completion =  {menu = { auto_show = true; };};
+        completion = { menu = { auto_show = true; }; };
         keymap = {
           "<C-space>" = [ "show" "show_documentation" "hide_documentation" ];
           "<C-e>" = [ "hide" ];
           "<C-y>" = [ "select_and_accept" ];
-          "<C-p>" = [ "select_prev" "fallback" ];
-          "<C-n>" = [ "select_next" "fallback" ];
+          "<C-p>" = [ "show" "select_prev" "fallback" ];
+          "<C-n>" = [ "show" "select_next" "fallback" ];
           "<C-b>" = [ "scroll_documentation_up" "fallback" ];
           "<C-f>" = [ "scroll_documentation_down" "fallback" ];
           "<Tab>" = [ "snippet_forward" "fallback" ];
