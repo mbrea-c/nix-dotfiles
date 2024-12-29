31c44f63c728c30d99f1f45638bb35bbc76f4fb2
Manuel Brea Carreras <m.brea.carreras@gmail.com>
2024-12-29 12:56:12 +0000

    switch to blink

---
+++ modules/nixvim/modules/plugins/blink.nix
@@ -0,0 +1,120 @@
+{ ... }: {
+  plugins = {
+    blink-cmp = {
+      enable = true;
+      settings = {
+        menu = { auto_show = true; };
+        keymap = {
+          "<C-space>" = [ "show" "show_documentation" "hide_documentation" ];
+          "<C-e>" = [ "hide" ];
+          "<C-y>" = [ "select_and_accept" ];
+          "<C-p>" = [ "select_prev" "fallback" ];
+          "<C-n>" = [ "select_next" "fallback" ];
+          "<C-b>" = [ "scroll_documentation_up" "fallback" ];
+          "<C-f>" = [ "scroll_documentation_down" "fallback" ];
+          "<Tab>" = [ "snippet_forward" "fallback" ];
+          "<S-Tab>" = [ "snippet_backward" "fallback" ];
+        };
+      };
+    };
+    # cmp = {
+    #   enable = true;
+    #   autoEnableSources = true;
+    #   settings = {
+    #     sources = [
+    #       {
+    #         name = "copilot";
+    #       }
+    #       #{ name = "cmp_ai"; }
+    #       { name = "nvim_lsp"; }
+    #       { name = "path"; }
+    #       { name = "buffer"; }
+    #     ];
+    #     window = {
+    #       completion = {
+    #         col_offset = -3;
+    #         side_padding = 0;
+    #         border = "rounded";
+    #       };
+    #     };
+    #     formatting = {
+    #       fields = [ "abbr" "kind" "menu" ];
+    #       format = # lua
+    #         ''
+    #           function(entry, vim_item)
+    #             local kind_icons = {
+    #               Text = "",
+    #               Method = "󰆧",
+    #               Function = "󰊕",
+    #               Constructor = "",
+    #               Field = "󰇽",
+    #               Variable = "󰂡",
+    #               Class = "󰠱",
+    #               Interface = "",
+    #               Module = "",
+    #               Property = "󰜢",
+    #               Unit = "",
+    #               Value = "󰎠",
+    #               Enum = "",
+    #               Keyword = "󰌋",
+    #               Snippet = "",
+    #               Color = "󰏘",
+    #               File = "󰈙",
+    #               Reference = "",
+    #               Folder = "󰉋",
+    #               EnumMember = "",
+    #               Constant = "󰏿",
+    #               Struct = "",
+    #               Event = "",
+    #               Operator = "󰆕",
+    #               TypeParameter = "󰅲",
+    #               Copilot = "",
+    #             }
+    #             local source_map = {
+    #               buffer = "[Buffer]",
+    #               nvim_lsp = "[LSP]",
+    #               luasnip = "[LuaSnip]",
+    #               nvim_lua = "[Lua]",
+    #               latex_symbols = "[LaTeX]",
+    #             }
+
+    #             vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+    #             if source_map[vim_item.menu] ~= nil then
+    #               vim_item.menu = source_map[vim_item.menu]
+    #             end
+
+    #             return vim_item
+    #           end
+    #         '';
+    #     };
+    #     mapping = {
+    #       "<CR>" = "cmp.mapping.confirm({ select = false })";
+    #       "<C-n>" = # lua
+    #         ''
+    #           cmp.mapping(function(fallback)
+    #             if cmp.visible() then
+    #               cmp.select_next_item()
+    #             else
+    #               cmp.complete()
+    #             end
+    #           end, { "i", "s" })
+    #         '';
+    #       "<C-p>" = "cmp.mapping.select_prev_item()";
+    #       "<ESC>" = # lua
+    #         ''
+    #           cmp.mapping(function(fallback)
+    #             if cmp.visible() then
+    #               cmp.abort()
+    #               -- Emulate <Esc> keypress after exiting, so that you're not left in
+    #               -- insert mode
+    #               vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
+    #             else
+    #               fallback()
+    #             end
+    #           end, { "i", "c" })
+    #         '';
+    #     };
+    #   };
+    # };
+  };
+}
