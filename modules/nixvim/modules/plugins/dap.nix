3fb4d0fc0f1405a1b35dec23285b2a428002fbf6
Manuel Brea <m.brea.carreras@gmail.com>
2025-02-19 19:07:12 +0000

    fi

--- modules/nixvim/modules/plugins/dap.nix
+++ modules/nixvim/modules/plugins/dap.nix
@@ -29,6 +29,11 @@ in {
           env = { };
           terminalKind = "integrated";
         }];
+        extensions = {
+          dap-ui = { enable = true; };
+          dap-virtual-text = { enable = true; };
+        };
+
         # kotlin = [{
         #   type = "kotlin";
         #   request = "launch";
@@ -49,4 +54,133 @@ in {
       };
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>db";
+      action.__raw = ''
+        function()
+          require("dap").toggle_breakpoint()
+        end
+      '';
+      options = {
+        desc = "Breakpoint toggle";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>dc";
+      action.__raw = ''
+        function()
+          require("dap").continue()
+        end
+      '';
+      options = {
+        desc = "Continue Debugging (Start)";
+        silent = true;
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>de";
+      action.__raw = ''
+        function() require("dapui").eval() end
+      '';
+      options = {
+        desc = "Evaluate Input";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>de";
+      action.__raw = ''
+        function()
+          vim.ui.input({ prompt = "Expression: " }, function(expr)
+            if expr then require("dapui").eval(expr, { enter = true }) end
+          end)
+        end
+      '';
+      options = {
+        desc = "Evaluate Input";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>dh";
+      action.__raw = ''
+        function() require("dap.ui.widgets").hover() end
+      '';
+      options = {
+        desc = "Debugger Hover";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>do";
+      action.__raw = ''
+        function()
+          require("dap").step_out()
+        end
+      '';
+      options = {
+        desc = "Step Out";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ds";
+      action.__raw = ''
+        function()
+          require("dap").step_over()
+        end
+      '';
+      options = {
+        desc = "Step Over";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>dS";
+      action.__raw = ''
+        function()
+          require("dap").step_into()
+        end
+      '';
+      options = {
+        desc = "Step Into";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>dt";
+      action.__raw = ''
+        function() require("dap").terminate() end
+      '';
+      options = {
+        desc = "Terminate Debugging";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>du";
+      action.__raw = ''
+        function()
+          require('dap.ext.vscode').load_launchjs(nil, {})
+          require("dapui").toggle()
+        end
+      '';
+      options = {
+        desc = "Toggle Debugger UI";
+        silent = true;
+      };
+    }
+  ];
 }
