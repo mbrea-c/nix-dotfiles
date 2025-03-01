4d5527ed3a8e2ed6a17b7cb10cd53bf2da8de5a8
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-01 19:21:46 +0000

    fix

--- hosts/pivot.nix
+++ hosts/pivot.nix
@@ -109,8 +109,4 @@
 
   programs.ssh.startAgent = true;
   programs.zsh.enable = true;
-
-  # DO NOT CHANGE:
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
 }
--- templates/pivot/setup.sh
+++ templates/pivot/setup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+DISK_NAME="main"
+DISK_DEVICE="$1"
+
+nixos-generate-config --root . --no-filesystems
+sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#pivot --disk "$DISK_NAME" "$DISK_DEVICE"
