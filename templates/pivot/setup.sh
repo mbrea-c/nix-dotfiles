c2ffcd1a9cc7edae4afb3b374ed0201478d6b5c5
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-01 20:16:27 +0000

    fix

--- templates/pivot/setup.sh
+++ templates/pivot/setup.sh
@@ -3,6 +3,11 @@
 DISK_NAME="main"
 DISK_DEVICE="$1"
 
+# get configs ready
 nixos-generate-config --root . --no-filesystems
 mv ./etc/nixos/{configuration,hardware-configuration}.nix ./
-sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake .#pivot --disk "$DISK_NAME" "$DISK_DEVICE"
+
+# mount stuff
+sudo nix --experimental-features "nix-command flakes" run "github:nix-community/disko/latest" -- --flake .#pivot --arg diskMappings "{ \"${DISK_NAME}\" = \"${DISK_DEVICE}\"; }" --mode destroy,format,mount
+
+nixos-install --flake .#pivot
