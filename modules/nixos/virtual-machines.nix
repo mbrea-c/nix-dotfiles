e79be9bff4b9070f4ae553d399ed4265ee947065
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-04 21:27:08 +0000

    fix

--- modules/nixos/virtual-machines.nix
+++ modules/nixos/virtual-machines.nix
@@ -2,7 +2,10 @@
   programs.virt-manager.enable = true;
 
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = { ovmf.enable = true; };
+    };
     spiceUSBRedirection.enable = true;
   };
 
