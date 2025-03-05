693ec90bcd923be3aae452e0bb935c0164d83104
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-05 20:16:18 +0000

    fix

--- modules/nixvim/modules/plugins/direnv.nix
+++ modules/nixvim/modules/plugins/direnv.nix
@@ -1,1 +1,8 @@
-{ ... }: { plugins = { direnv = { enable = true; }; }; }
+{ config, ... }: {
+  plugins = {
+    direnv = {
+      enable = true;
+      settings = { input = { border = config.border; }; };
+    };
+  };
+}
