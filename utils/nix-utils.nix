204fd0e3f517fdd100cf5cbf27c90a6c504f44b1
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-28 15:04:56 +0000

    fix

--- utils/nix-utils.nix
+++ utils/nix-utils.nix
@@ -1,7 +1,7 @@
 { lib, ... }: {
-  compose = fns:
-    lib.lists.foldr (fn: acc: (target: builtins.trace fn (fn (acc target))))
-    (x: x);
+  compose = fns: target:
+    (lib.lists.foldr (fn: acc: (target: builtins.trace fn (fn (acc target))))
+      (x: x) fns) target;
   attrsToList = attrs:
     lib.mapAttrsToList (key: value: { inherit key value; }) attrs;
 }
