f96d83dad15e177395b795b60d556dc8e3677216
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-28 14:50:01 +0000

    fix

--- utils/nix-utils.nix
+++ utils/nix-utils.nix
@@ -1,6 +1,6 @@
 { ... }: {
   compose = fns: target:
-    builtins.foldl' (acc: fn: target: acc (fn target)) (x: x);
+    builtins.foldl' (acc: fn: target: acc (fn target)) (x: x) target;
   attrsToList = attrs:
     builtins.attrNames
     (builtins.mapAttrs (key: value: { inherit key value; }) attrs);
