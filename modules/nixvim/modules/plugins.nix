66e71bd61259a3b21a51af6bb1f91489ccf4992b
Manuel Brea <m.brea.carreras@gmail.com>
2025-03-28 14:24:09 +0000

    fix

--- modules/nixvim/modules/plugins.nix
+++ modules/nixvim/modules/plugins.nix
@@ -1,9 +1,13 @@
 # This module just imports all of the active plugin modules
-{ ... }:
+{ lib, ... }:
 let
+  nu = ../../../utils/nix-utils.nix;
   directory = ./plugins;
-  files = builtins.map ({ key, ... }: directory + "/${key}")
-    (builtins.filter ({ value, ... }: value == "regular") (builtins.attrValues
-      (builtins.mapAttrs (key: value: { inherit key value; })
-        (builtins.readDir directory))));
+  files = (nu.compose [
+    (builtins.filter (lib.strings.hasSuffix ".nix"))
+    (builtins.map ({ key, ... }: directory + "/${key}"))
+    (builtins.filter ({ value, ... }: value == "regular"))
+    builtins.attrValues
+    (builtins.mapAttrs (key: value: { inherit key value; }))
+  ]) (builtins.readDir directory);
 in { imports = files; }
