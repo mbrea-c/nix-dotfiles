0c1127f3a8be34fb0350308a3b5f552375fdbf8d
Manuel Brea <m.brea.carreras@gmail.com>
2025-04-05 13:17:26 +0100

    simplify plugins root

--- utils/nix-utils.nix
+++ utils/nix-utils.nix
@@ -1,6 +1,13 @@
-{ lib, ... }: {
+{ lib, ... }: rec {
   compose = fns: target:
     (lib.lists.foldr (fn: acc: target: fn (acc target)) (x: x) fns) target;
   attrsToList = attrs:
     lib.mapAttrsToList (key: value: { inherit key value; }) attrs;
+  allFilesInDir = suffix: directory:
+    compose [
+      (builtins.map ({ key, ... }: directory + "/${key}"))
+      (builtins.filter ({ key, ... }: lib.strings.hasSuffix suffix key))
+      (builtins.filter ({ value, ... }: value == "regular"))
+      attrsToList
+    ] (builtins.readDir directory);
 }
