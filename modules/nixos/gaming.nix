ae74b69458010a08244a866e51bb5ab2d62249d0
Manuel Brea <m.brea.carreras@gmail.com>
2025-05-02 20:17:58 +0100

    fix

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -5,10 +5,17 @@
     enable = true;
     capSysNice = true;
   };
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+  };
 
   environment.systemPackages = [
     (pkgs.lutris.override {
       extraLibraries = pkgs: [ ];
+      extraPkgs = pkgs: [ pkgs.gamescope pkgs.mangohud ];
+    })
+    (pkgs.heroic.override {
       extraPkgs = pkgs: [ pkgs.gamescope pkgs.mangohud ];
     })
   ];
