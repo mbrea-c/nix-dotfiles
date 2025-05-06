e2f87f91d188a62df8360aa42db1d486946b8b43
Manuel Brea Carreras <mmanuelc@amazon.com>
2025-05-06 16:35:49 +0100

    fix

--- modules/nixvim/plugins/avante.nix
+++ modules/nixvim/plugins/avante.nix
@@ -1,8 +1,8 @@
 { lib, avanteSettings ? { }, ... }: {
   plugins = {
-    avante = {
+    avante = lib.attrsets.recursiveUpdate {
       enable = true;
-      settings = lib.attrsets.recursiveUpdate {
+      settings = {
         mappings = {
           ask = "<leader><leader>a";
           edit = "<leader><leader>e";
@@ -16,6 +16,13 @@
             suggestion = "<leader><leader>s";
             repomap = "<leader><leader>R";
           };
+          files = {
+            add_current = "<leader><leader>c";
+            add_all_buffers = "<leader><leader>B";
+          };
+          select_model = "<leader><leader>?";
+          select_history = "<leader><leader>h";
+
           diff = {
             next = "g>";
             prev = "g<";
@@ -25,11 +32,11 @@
             both = "cb";
           };
         };
-      } avanteSettings;
+      };
 
       luaConfig.pre = ''
         ${builtins.readFile ./avante_bedrock_keys.lua}
       '';
-    };
+    } avanteSettings;
   };
 }
