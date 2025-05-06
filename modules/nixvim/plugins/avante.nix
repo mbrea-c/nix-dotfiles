d77203ea95388faa044b817a24a76168655bf91f
Manuel Brea Carreras <mmanuelc@amazon.com>
2025-05-06 14:42:27 +0100

    fix avante keymap

--- modules/nixvim/plugins/avante.nix
+++ modules/nixvim/plugins/avante.nix
@@ -2,7 +2,18 @@
   plugins = {
     avante = {
       enable = true;
-      settings = lib.attrsets.recursiveUpdate { } avanteSettings;
+      settings = lib.attrsets.recursiveUpdate {
+        mappings = {
+          diff = {
+            next = "<leader><leader>>";
+            prev = "<leader><leader><";
+            none = "<leader><leader>c0";
+            ours = "<leader><leader>co";
+            theirs = "<leader><leader>ct";
+            both = "<leader><leader>cb";
+          };
+        };
+      } avanteSettings;
     };
   };
 }
