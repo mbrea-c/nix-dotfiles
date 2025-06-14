cb0fde68a8f9d3c53e1fec84cae9eb56704dcecc
Manuel Brea <m.brea.carreras@gmail.com>
2025-06-14 11:24:57 +0100

    fix

--- utils/lua-utils.nix
+++ utils/lua-utils.nix
@@ -35,7 +35,7 @@
           local ignore_formatting = ${nixToLua ignoredServers}
           vim.lsp.buf.format({
             filter = function(client)
-              return not vim.tbl_contains(client.name, ignore_formatting)
+              return not vim.tbl_contains(ignore_formatting, client.name)
             end,
             bufnr = bufnr,
             timeout_ms = 20000,
