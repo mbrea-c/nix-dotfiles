3d3b26ca67f3a0ee43ecfd08cc5799f2395969f4
Manuel Brea <m.brea.carreras@gmail.com>
2025-08-15 19:07:17 +0100

    fix

--- modules/nixvim/plugins/nvim-tree.nix
+++ modules/nixvim/plugins/nvim-tree.nix
@@ -2,24 +2,26 @@
   plugins = {
     nvim-tree = {
       enable = true;
-      renderer = {
-        groupEmpty = true;
-        highlightOpenedFiles = "all";
-        indentMarkers = { enable = true; };
+      settings = {
+        renderer = {
+          groupEmpty = true;
+          highlightOpenedFiles = "all";
+          indentMarkers = { enable = true; };
+        };
+        diagnostics = {
+          enable = true;
+          showOnDirs = true;
+          showOnOpenDirs = false;
+        };
+        git = {
+          enable = true;
+          showOnDirs = true;
+          showOnOpenDirs = false;
+        };
+        updateFocusedFile = { enable = true; };
+        view = { width = 50; };
+        actions = { changeDir = { global = true; }; };
       };
-      diagnostics = {
-        enable = true;
-        showOnDirs = true;
-        showOnOpenDirs = false;
-      };
-      git = {
-        enable = true;
-        showOnDirs = true;
-        showOnOpenDirs = false;
-      };
-      updateFocusedFile = { enable = true; };
-      view = { width = 50; };
-      actions = { changeDir = { global = true; }; };
     };
   };
 }
