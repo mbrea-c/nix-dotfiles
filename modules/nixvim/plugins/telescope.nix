95154d1b67ee2e81ccf735606a617934a7b1142a
Manuel Brea Carreras <mmanuelc@amazon.com>
2025-10-16 14:02:54 +0100

    feat: add keymaps for telescope history navigation

--- modules/nixvim/plugins/telescope.nix
+++ modules/nixvim/plugins/telescope.nix
@@ -3,6 +3,22 @@
     telescope = {
       enable = true;
       extensions = { live-grep-args = { enable = true; }; };
+      settings = {
+        defaults = {
+          mappings = {
+            i = {
+              "<Up>".__raw = "require('telescope.actions').cycle_history_prev";
+              "<Down>".__raw =
+                "require('telescope.actions').cycle_history_next";
+            };
+            n = {
+              "<Up>".__raw = "require('telescope.actions').cycle_history_prev";
+              "<Down>".__raw =
+                "require('telescope.actions').cycle_history_next";
+            };
+          };
+        };
+      };
     };
   };
 }
