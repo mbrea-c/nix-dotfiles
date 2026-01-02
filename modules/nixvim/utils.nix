ae75adca8867291e18e8338759f8fe88cf921b39
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-02 23:11:28 +0000

    fix

--- modules/nixvim/utils.nix
+++ modules/nixvim/utils.nix
@@ -1,10 +1,5 @@
-{ lib, ... }:
-{
-  setTabstopForFiletypes =
-    {
-      filetypes,
-      augroup ? "TabstopForFiletypes",
-    }:
+{ lib, ... }: {
+  setTabstopForFiletypes = { filetypes, augroup ? "TabstopForFiletypes", }:
     lib.attrsets.mapAttrsToList (lang: tabstop: {
       group = augroup;
       event = "Filetype";
@@ -19,39 +14,32 @@
     }) filetypes;
 
   setFormatOnSaveForFiletypes =
-    {
-      filetypes,
-      augroupEnabler,
-      augroupFormatter,
-      formatFn,
-    }:
+    { filetypes, augroupEnabler, augroupFormatter, formatFn, }:
     lib.attrsets.mapAttrsToList (lang: enable: {
       group = augroupEnabler;
       event = "Filetype";
       callback = {
-        __raw =
-          if enable then # lua
-            ''
-              function(ev) 
-                local augroup = vim.api.nvim_create_augroup("${augroupFormatter}${lang}" .. ev.buf, { clear = true })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                  group = augroup,
-                  buffer = ev.buf,
-                  callback = function()
-                    ${formatFn}(),
-                  end,
-                })
-              end
-            ''
-          # lua
-          else
-            ''
-              function(ev)
-                vim.api.nvim_clear_autocmds({
-                  group = "${augroupFormatter}${lang}" .. ev.buf,
-                })
-              end
-            '';
+        __raw = if enable then # lua
+        ''
+          function(ev) 
+            local augroup = vim.api.nvim_create_augroup("${augroupFormatter}${lang}" .. ev.buf, { clear = true })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = ev.buf,
+              callback = function()
+                (${formatFn})(),
+              end,
+            })
+          end
+        ''
+        # lua
+        else ''
+          function(ev)
+            vim.api.nvim_clear_autocmds({
+              group = "${augroupFormatter}${lang}" .. ev.buf,
+            })
+          end
+        '';
       };
       pattern = [ lang ];
     }) filetypes;
