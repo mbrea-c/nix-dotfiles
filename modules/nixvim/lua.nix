8808c6172e0d59c743ccdae25fa37db90574c748
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-02 23:36:43 +0000

    fix

--- modules/nixvim/lua.nix
+++ modules/nixvim/lua.nix
@@ -6,10 +6,10 @@ let
   files = nu.allFilesInDir ".lua" directory;
 in {
   extraFiles = builtins.trace (builtins.listToAttrs (map (file: {
-    name = "lua/" ++ (builtins.baseNameOf file);
+    name = "lua/" + (builtins.baseNameOf file);
     value = { source = file; };
   }) files)) (builtins.listToAttrs (map (file: {
-    name = "lua/" ++ (builtins.baseNameOf file);
+    name = "lua/" + (builtins.baseNameOf file);
     value = { source = file; };
   }) files));
 }
