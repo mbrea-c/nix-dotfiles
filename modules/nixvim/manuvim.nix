b929fab4e1d1322cce53c99bb94cb3d629eeef7c
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-02 23:38:59 +0000

    fix this time

--- modules/nixvim/manuvim.nix
+++ modules/nixvim/manuvim.nix
@@ -8,7 +8,7 @@ let
   highlightUnderCursorAugroup = "HighlightSymbolUnderCursor";
   highlights = (import ./colorscheme.nix) { inherit lib colorscheme inputs; };
 in {
-  imports = [ ./plugins.nix ./keymap.nix ./opts.nix ./packages.nix ];
+  imports = [ ./plugins.nix ./keymap.nix ./opts.nix ./packages.nix ./lua.nix ];
 
   options = {
     custom = {
