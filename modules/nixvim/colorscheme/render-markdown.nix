b8a081156117b7ec72514698ccb5778ebd2558fd
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-03 19:25:43 +0000

    fix

--- modules/nixvim/colorscheme/render-markdown.nix
+++ modules/nixvim/colorscheme/render-markdown.nix
@@ -2,13 +2,5 @@
 {
   custom.colorscheme = {
     # The "Bg" highlights extend through the entire line. The regular highlights don't
-    "RenderMarkdownH3Bg" = {
-    };
-    "RenderMarkdownH4Bg" = {
-    };
-    "RenderMarkdownH5Bg" = {
-    };
-    "RenderMarkdownH6Bg" = {
-    };
   };
 }
