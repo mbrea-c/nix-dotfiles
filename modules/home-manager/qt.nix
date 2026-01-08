215295eff1a11ebbf19d8aad6a6546891bdd94d7
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-08 20:59:04 +0000

    fix

--- modules/home-manager/qt.nix
+++ modules/home-manager/qt.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    libsForQt5.qt5ct
     kdePackages.qt6ct
-    breeze-qt5
-    breeze-qt6
-    breeze-icons
+    kdePackages.breeze
+    kdePackages.breeze-icons
   ];
 
   home.sessionVariables = {
     QT_QPA_PLATFORM = "wayland";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_QPA_PLATFORMTHEME = "qt6ct";
   };
 }
