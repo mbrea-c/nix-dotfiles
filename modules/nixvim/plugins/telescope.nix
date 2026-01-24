7015db3c4e21ca5a547477019a47f0257ef43a5b
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-24 17:21:19 +0000

    fix

--- modules/nixvim/plugins/telescope.nix
+++ modules/nixvim/plugins/telescope.nix
@@ -1,17 +1,22 @@
 { ... }:
-let desc = d: { desc = d; };
-in {
+let
+  desc = d: { desc = d; };
+in
+{
   plugins = {
     telescope = {
       enable = true;
-      extensions = { live-grep-args = { enable = true; }; };
+      extensions = {
+        live-grep-args = {
+          enable = true;
+        };
+      };
       settings = {
         defaults = {
           mappings = {
             i = {
               "<Up>".__raw = "require('telescope.actions').cycle_history_prev";
-              "<Down>".__raw =
-                "require('telescope.actions').cycle_history_next";
+              "<Down>".__raw = "require('telescope.actions').cycle_history_next";
               "<C-1>".__raw = ''
                 function(prompt_bufnr)
                   require("telescope.actions.generate").refine(prompt_bufnr, {
@@ -23,8 +28,7 @@ in {
             };
             n = {
               "<Up>".__raw = "require('telescope.actions').cycle_history_prev";
-              "<Down>".__raw =
-                "require('telescope.actions').cycle_history_next";
+              "<Down>".__raw = "require('telescope.actions').cycle_history_next";
               "<C-1>".__raw = ''
                 function(prompt_bufnr)
                   require("telescope.actions.generate").refine(prompt_bufnr, {
@@ -52,9 +56,23 @@ in {
       action = "<cmd>Telescope find_files<CR>";
       options = desc "Find files in workspace";
     }
+    {
+      mode = [ "n" ];
+      key = "<leader>ssw";
+      action = "<cmd>Telescope lsp_workspace_symbols<CR>";
+      options = desc "Find symbols in workspace";
+    }
+    {
+      mode = [ "n" ];
+      key = "<leader>ssd";
+      action = "<cmd>Telescope lsp_document_symbols<CR>";
+      options = desc "Find symbols in file";
+    }
   ];
-  custom.which-key.spec = [{
-    __unkeyed-1 = "<leader>s";
-    desc = "Telescope";
-  }];
+  custom.which-key.spec = [
+    {
+      __unkeyed-1 = "<leader>s";
+      desc = "Telescope";
+    }
+  ];
 }
