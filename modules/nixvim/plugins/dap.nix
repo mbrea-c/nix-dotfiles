c164ce2707cf889d347d4bab2f7da5d6fa014f92
Manuel Brea <m.brea.carreras@gmail.com>
2026-01-30 22:05:34 +0000

    fix

--- modules/nixvim/plugins/dap.nix
+++ modules/nixvim/plugins/dap.nix
@@ -23,18 +23,45 @@ let
       "--asset-source"
       "assets"
     ];
-  };
-
-  codelldb-config = {
-    name = "Launch (CodeLLDB)";
-    type = "codelldb";
-    request = "launch";
-    program.__raw = ''
+    initCommands.__raw = ''
       function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. '/', "file")
-      end'';
-    cwd = "\${workspaceFolder}";
-    stopOnEntry = false;
+        -- Find out where to look for the pretty printer Python module.
+        local rustc_sysroot = vim.fn.trim(vim.fn.system 'rustc --print sysroot')
+        assert(
+          vim.v.shell_error == 0,
+          'failed to get rust sysroot using `rustc --print sysroot`: '
+            .. rustc_sysroot
+        )
+        local script_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py'
+        local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+        -- The following is a table/list of lldb commands, which have a syntax
+        -- similar to shell commands.
+        --
+        -- To see which command options are supported, you can run these commands
+        -- in a shell:
+        --
+        --   * lldb --batch -o 'help command script import'
+        --   * lldb --batch -o 'help command source'
+        --
+        -- Commands prefixed with `?` are quiet on success (nothing is written to
+        -- debugger console if the command succeeds).
+        --
+        -- Prefixing a command with `!` enables error checking (if a command
+        -- prefixed with `!` fails, subsequent commands will not be run).
+        --
+        -- NOTE: it is possible to put these commands inside the ~/.lldbinit
+        -- config file instead, which would enable rust types globally for ALL
+        -- lldb sessions (i.e. including those run outside of nvim). However,
+        -- that may lead to conflicts when debugging other languages, as the type
+        -- formatters are merely regex-matched against type names. Also note that
+        -- .lldbinit doesn't support the `!` and `?` prefix shorthands.
+        return {
+          ([[!command script import '%s']]):format(script_file),
+          ([[command source '%s']]):format(commands_file),
+        }
+      end
+    '';
   };
 
   sh-config = {
@@ -71,18 +98,6 @@ in
             command = lib.getExe' pkgs.lldb "lldb-dap";
           };
         };
-        servers = {
-          codelldb = lib.mkIf pkgs.stdenv.isLinux {
-            port = 13000;
-            executable = {
-              command = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
-              args = [
-                "--port"
-                "13000"
-              ];
-            };
-          };
-        };
       };
       configurations = {
         sh = [
@@ -90,7 +105,6 @@ in
         ];
         rust = [
           lldb-config
-          codelldb-config
           lldb-config-animedit
         ];
 
