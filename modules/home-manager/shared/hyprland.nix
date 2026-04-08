c9478303919812ea4995398b2e45015e987ab0e8
Manuel Brea <m.brea.carreras@gmail.com>
2026-04-08 17:35:52 +0100

    hyprland config

--- modules/home-manager/shared/hyprland.nix
+++ modules/home-manager/shared/hyprland.nix
@@ -17,6 +17,26 @@ in
     package = null;
 
     systemd.enable = true;
+
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, Return, exec, kitty"
+        "$mod SHIFT, C, reload"
+      ]
+      ++ builtins.concatLists (
+        builtins.genList (
+          i:
+          let
+            ws = i + 1;
+          in
+          [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        ) 9
+      );
+    };
   };
 
   systemd.user.services = {
